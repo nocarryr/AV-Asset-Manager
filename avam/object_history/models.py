a116e7f45deb6f7389d55d124e37d6e3de671b36
nocarryr <matt@nomadic-recording.com>
2015-12-23 14:10:37 -0600

    Iterate for one_to_many rel

--- avam/object_history/models.py
+++ avam/object_history/models.py
@@ -18,7 +18,7 @@ def iter_fields(obj, query_lookup=None):
         if f.is_relation:
             content_type = ContentType.objects.get_for_model(f.related_model)
             watched = WatchedModel.objects.filter(content_type=content_type).exists()
-            if f.many_to_many:
+            if f.many_to_many or f.one_to_many:
                 for _obj in getattr(obj, f.name).all():
                     if False:#not watched:
                         yield iter_fields(_obj, fname)
