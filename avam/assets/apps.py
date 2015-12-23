21c6127889894b74ef762bc58a834ba96774982e
nocarryr <matt@nomadic-recording.com>
2015-12-23 13:52:36 -0600

    Tie object_history into assets to begin debugging

--- avam/assets/apps.py
+++ avam/assets/apps.py
@@ -5,3 +5,6 @@ class AssetsConfig(AppConfig):
     name = 'assets'
     def ready(self):
         import assets.signals
+        from django.apps import apps
+        object_history = apps.get_app_config('object_history').models_module
+        object_history.add_model_history(*[m for m in self.get_models()])
