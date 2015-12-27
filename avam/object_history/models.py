9248fcffa3a1298bc3c64c0a12b851bb33e067da
nocarryr <matt@nomadic-recording.com>
2015-12-27 12:47:57 -0600

    Separate utility functions into utils module

--- avam/object_history/models.py
+++ avam/object_history/models.py
@@ -1,40 +1,13 @@
 from __future__ import unicode_literals
 
-import datetime
-
 from django.conf import settings
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
-from django.utils import timezone
 
-def iter_fields(obj, query_lookup=None):
-    m = obj._meta
-    for f in m.get_fields():
-        if query_lookup is None:
-            fname = f.name
-        else:
-            fname = '__'.join([query_lookup, f.name])
-        if f.is_relation:
-            if f.many_to_many or f.one_to_many:
-                for _obj in getattr(obj, f.name).all():
-                    yield m, fname, f, _obj.pk
-            else:
-                yield m, fname, f, getattr(obj, f.name).pk
-        else:
-            yield m, fname, f, getattr(obj, f.name)
-    
-def get_query_value(obj, query_lookup):
-    if '__' not in query_lookup:
-        value = getattr(obj, query_lookup)
-        f = obj._meta.get_field(query_lookup)
-        if f.is_relation:
-            return value.pk
-        else:
-            return value
-    attr = query_lookup.split('__')[0]
-    query_lookup = '__'.join(query_lookup.split('__')[1:])
-    return get_query_value(getattr(obj, attr), query_lookup)
+from object_history.utils import (
+    iter_fields, get_query_value, value_to_str, str_to_value
+)
 
 class WatchedModel(models.Model):
     content_type = models.ForeignKey(ContentType)
@@ -107,49 +80,6 @@ class ObjectUpdate(models.Model):
     def __unicode__(self):
         return u'%s: %s' % (self.content_object, self.datetime)
 
-PY_TYPES = dict(
-    int=int,
-    str=str,
-    unicode=unicode,
-    float=float,
-    bool=bool,
-    NoneType=None,
-)
-
-def str_to_value(s, py_type):
-    if 'datetime' in py_type:
-        dtcls = py_type.split('.')[1]
-        if dtcls == 'datetime':
-            dt_fmt = '%Y-%m-%d %H:%M:%S'
-            if '.' in s:
-                dt_fmt = '.'.join([dt_fmt, '%f'])
-            dt_fmt = '+'.join([dt_fmt, '00:00'])
-            dt = datetime.datetime.strptime(s, dt_fmt)
-            value = timezone.utc.localize(dt)
-        elif dtcls == 'time':
-            if '.' in s:
-                s = ':'.join(s.split('.'))
-            args = [int(arg) for arg in s.split(':')]
-            value = datetime.time(*args)
-        elif dtcls == 'date':
-            args = [int(arg) for arg in s.split('-')]
-            value = datetime.date(*args)
-        elif dtcls == 'timedelta':
-            value = datetime.timedelta(float(s))
-    else:
-        if py_type == 'bool':
-            return {'True':True, 'False':False}.get(s)
-        py_type = PY_TYPES.get(py_type)
-        if py_type is None:
-            value = None
-        else:
-            value = py_type(s)
-    return value
-
-def value_to_str(value):
-    if isinstance(value, datetime.timedelta):
-        value = value.total_seconds()
-    return unicode(value)
 
 class ObjectChange(models.Model):
     update = models.ForeignKey(ObjectUpdate, related_name='changes')
---
+++ avam/object_history/utils.py
@@ -0,0 +1,75 @@
+import datetime
+
+from django.utils import timezone
+
+def iter_fields(obj, query_lookup=None):
+    m = obj._meta
+    for f in m.get_fields():
+        if query_lookup is None:
+            fname = f.name
+        else:
+            fname = '__'.join([query_lookup, f.name])
+        if f.is_relation:
+            if f.many_to_many or f.one_to_many:
+                for _obj in getattr(obj, f.name).all():
+                    yield m, fname, f, _obj.pk
+            else:
+                yield m, fname, f, getattr(obj, f.name).pk
+        else:
+            yield m, fname, f, getattr(obj, f.name)
+    
+def get_query_value(obj, query_lookup):
+    if '__' not in query_lookup:
+        value = getattr(obj, query_lookup)
+        f = obj._meta.get_field(query_lookup)
+        if f.is_relation:
+            return value.pk
+        else:
+            return value
+    attr = query_lookup.split('__')[0]
+    query_lookup = '__'.join(query_lookup.split('__')[1:])
+    return get_query_value(getattr(obj, attr), query_lookup)
+
+PY_TYPES = dict(
+    int=int,
+    str=str,
+    unicode=unicode,
+    float=float,
+    bool=bool,
+    NoneType=None,
+)
+
+def str_to_value(s, py_type):
+    if 'datetime' in py_type:
+        dtcls = py_type.split('.')[1]
+        if dtcls == 'datetime':
+            dt_fmt = '%Y-%m-%d %H:%M:%S'
+            if '.' in s:
+                dt_fmt = '.'.join([dt_fmt, '%f'])
+            dt_fmt = '+'.join([dt_fmt, '00:00'])
+            dt = datetime.datetime.strptime(s, dt_fmt)
+            value = timezone.utc.localize(dt)
+        elif dtcls == 'time':
+            if '.' in s:
+                s = ':'.join(s.split('.'))
+            args = [int(arg) for arg in s.split(':')]
+            value = datetime.time(*args)
+        elif dtcls == 'date':
+            args = [int(arg) for arg in s.split('-')]
+            value = datetime.date(*args)
+        elif dtcls == 'timedelta':
+            value = datetime.timedelta(float(s))
+    else:
+        if py_type == 'bool':
+            return {'True':True, 'False':False}.get(s)
+        py_type = PY_TYPES.get(py_type)
+        if py_type is None:
+            value = None
+        else:
+            value = py_type(s)
+    return value
+
+def value_to_str(value):
+    if isinstance(value, datetime.timedelta):
+        value = value.total_seconds()
+    return unicode(value)
