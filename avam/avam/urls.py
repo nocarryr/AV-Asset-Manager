80604157eb79fd2fe257c9398bae9bdf2275fc6f
nocarryr <matt@nomadic-recording.com>
2015-12-28 14:13:14 -0600

    Add object history urls

--- avam/avam/urls.py
+++ avam/avam/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+
+from object_history import urls as object_history_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^object_history/',
+        include(
+            object_history_urls,
+            namespace='object_history',
+            app_name='object_history',
+        )
+    ),
 ]
