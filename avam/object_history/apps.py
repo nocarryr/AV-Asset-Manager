e96aa92fdb9d75396986dc7d451dc5233eda197c
nocarryr <matt@nomadic-recording.com>
2015-12-30 15:32:12 -0600

    Attach signals after post_migrate is fired

--- avam/object_history/apps.py
+++ avam/object_history/apps.py
@@ -1,7 +1,11 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+
+def on_post_migrate(sender, **kwargs):
+    import object_history.signals
 
 class ObjectHistoryConfig(AppConfig):
     name = 'object_history'
     def ready(self):
-        import object_history.signals
+        post_migrate.connect(on_post_migrate, sender=self)
