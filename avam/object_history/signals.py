954eac2f22aa07a814dab8c80be89c72c22a1673
nocarryr <matt@nomadic-recording.com>
2015-12-30 16:06:38 -0600

    Remove exception handlers to ensure post_migrate is working properly

--- avam/object_history/signals.py
+++ avam/object_history/signals.py
@@ -10,17 +10,9 @@ def on_all_post_save(sender, **kwargs):
         return
     if issubclass(sender, (WatchedModel, ObjectUpdate, ObjectChange)):
         return
-    try:
-        content_type = ContentType.objects.get_for_model(sender)
-    except RuntimeError as e:
-        if 'migrate' in str(e):
-            return
-    try:
-        if not WatchedModel.objects.filter(content_type=content_type).exists():
-            return
-    except Exception as e:
-        if 'no such table' in str(e):
-            return
+    content_type = ContentType.objects.get_for_model(sender)
+    if not WatchedModel.objects.filter(content_type=content_type).exists():
+        return
     obj = kwargs.get('instance')
     object_update = ObjectUpdate(content_object=obj)
     object_update.save()
