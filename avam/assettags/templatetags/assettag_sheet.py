ad7e10223a19432b1a4b86019412fd2d797d776c
nocarryr <matt@nomadic-recording.com>
2016-01-05 15:07:52 -0600

    Add templatetags for page handling

--- avam/assettags/templatetags/assettag_sheet.py
+++ avam/assettags/templatetags/assettag_sheet.py
@@ -11,6 +11,25 @@ def assettag_new_row(context):
         s = '</tr>\n<tr>'
     return mark_safe(s)
 
-#@register.simple_tag(takes_context=True)
-#def assettag_new_page(context):
-#    
+@register.simple_tag(takes_context=True)
+def assettag_new_page(context):
+    lines = []
+    if not context['forloop']['first']:
+        lines.extend([
+            '  </table>',
+            '</div>',
+        ])
+    lines.extend([
+        '<div class="tag-page">',
+        '  <table class="tag-table">',
+    ])
+    return mark_safe('\n'.join(lines))
+
+@register.simple_tag(takes_context=True)
+def assettag_endfor(context):
+    lines = [
+        '    </tr>',
+        '  </table>',
+        '</div>',
+    ]
+    return mark_safe('\n'.join(lines))
