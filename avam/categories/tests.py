c0e4097d69173a509ae96f3e93b5d6bbea3aec84
nocarryr <matt@nomadic-recording.com>
2016-01-18 17:31:47 -0600

    Add tests for categories

--- avam/categories/tests.py
+++ avam/categories/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.db import IntegrityError
 
-# Create your tests here.
+from categories.models import Category
+
+class CategoriesTestCase(TestCase):
+    def setUp(self):
+        base_names = ['root', 'branch', 'leaf']
+        for i in range(3):
+            parent = None
+            for base_name in base_names:
+                name = '{0}_{1}'.format(base_name, i)
+                category = Category(name=name, parent_category=parent)
+                category.save()
+                parent = category
+    def test_str(self):
+        root = Category.objects.get(name='root_1')
+        branch = root.subcategories.get(name='branch_1')
+        leaf = branch.subcategories.get(name='leaf_1')
+        self.assertEqual(str(leaf), 'root_1/branch_1/leaf_1')
+    def test_uniques(self):
+        root = Category.objects.get(name='root_1')
+        with self.assertRaises(IntegrityError):
+            bad_branch = Category(name='branch_1', parent_category=root)
+            bad_branch.save()
