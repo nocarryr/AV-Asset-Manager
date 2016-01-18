25c69bdf12823b25163068dcf10d79a91a61c41c
nocarryr <matt@nomadic-recording.com>
2016-01-18 15:56:37 -0600

    Set staticroot, gitignore it

--- avam/avam/settings.py
+++ avam/avam/settings.py
@@ -133,4 +133,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticroot')
+
 DATABASES.update(getattr(local_settings, 'DATABASES'))
