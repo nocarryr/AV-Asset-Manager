96bbd90a365e7e87215be852a037db0e9d7d509e
nocarryr <matt@nomadic-recording.com>
2016-01-19 11:00:32 -0600

    Add test case for category item assignment

--- avam/categories/tests.py
+++ avam/categories/tests.py
@@ -54,3 +54,48 @@ class CategoriesTestCase(TestCase):
         self.assertTrue(leaf.is_ancestor(branch))
         self.assertTrue(leaf.is_ancestor(root))
         self.assertFalse(leaf.is_ancestor(not_root))
+
+class CategoryItemTestCase(TestCase):
+    def setUp(self):
+        from assettypes.tests import build_test_fixtures as build_assettypes_fixures
+        assettypes_fixtures = build_assettypes_fixures()
+        category_fixtures = {}
+        for name in ['Video', 'Lighting', 'Accessories']:
+            category = Category.objects.create(name=name)
+            category_fixtures[name] = category
+            if name != 'Accessories':
+                sub_category = Category.objects.create(
+                    name='Accessories',
+                    parent_category=category,
+                )
+                category_fixtures[str(sub_category)] = sub_category
+        self.category_fixtures = category_fixtures
+        self.assettypes_fixtures = assettypes_fixtures
+    def get_fk_fields(self, obj):
+        for field in obj._meta.get_fields():
+            if not field.is_relation:
+                continue
+            if not field.one_to_many:
+                continue
+            yield field
+    def assign_items(self):
+        proj = self.assettypes_fixtures['projector']
+        manuf = proj.manufacturer
+        category = self.category_fixtures['Video']
+        category.add_item(manuf)
+        for f in self.get_fk_fields(manuf):
+            if not hasattr(f.related_model, 'manufacturer'):
+                continue
+            attr = f.get_accessor_name()
+            for obj in getattr(manuf, attr).all():
+                category.add_item(obj)
+    def test_assignment(self):
+        self.assign_items()
+        category = self.category_fixtures['Video']
+        q = category.get_items()
+        manuf = self.assettypes_fixtures['projector'].manufacturer
+        for category_item in q:
+            obj = category_item.content_object
+            if obj._meta.model_name == 'manufacturer':
+                continue
+            self.assertEqual(obj.manufacturer, manuf)
