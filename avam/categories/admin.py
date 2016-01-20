67de751503d204da5787392273431f8cf69ad770
nocarryr <matt@nomadic-recording.com>
2016-01-20 14:02:36 -0600

    Add category to admin

--- avam/categories/admin.py
+++ avam/categories/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from categories.models import Category
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
