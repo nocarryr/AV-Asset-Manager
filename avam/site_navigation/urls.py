b6b84562808c10fddb013ce3a1bc785f60d49fba
nocarryr <matt@nomadic-recording.com>
2016-01-28 09:19:29 -0600

    Add login/logout url conf and template

--- avam/site_navigation/urls.py
+++ avam/site_navigation/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 
+from django.contrib.auth import views as auth_views
+
 from site_navigation import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^accounts/login/$', auth_views.login, name='account_login'),
+    url(r'^accounts/logout/$', auth_views.logout, name='account_logout'),
 ]
