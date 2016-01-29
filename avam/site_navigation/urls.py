869a1056b1122d07ce2646e86346f8da22222d91
nocarryr <matt@nomadic-recording.com>
2016-01-29 09:54:24 -0600

    Add 'logged-out' template

--- avam/site_navigation/urls.py
+++ avam/site_navigation/urls.py
@@ -7,5 +7,9 @@ from site_navigation import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^accounts/login/$', auth_views.login, name='account_login'),
-    url(r'^accounts/logout/$', auth_views.logout, name='account_logout'),
+    url(r'^accounts/logout/$',
+        auth_views.logout,
+        {'template_name':'registration/logged-out.html'},
+        name='account_logout',
+    ),
 ]
