e290d4a532d39adf18aba355829a1ed0bd3ebb96
nocarryr <matt@nomadic-recording.com>
2016-02-01 11:35:59 -0600

    Attach signal handler on app.ready since its handler is working properly now

--- avam/object_history/apps.py
+++ avam/object_history/apps.py
@@ -1,11 +1,7 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
 
-
-def on_post_migrate(sender, **kwargs):
-    import object_history.signals
 
 class ObjectHistoryConfig(AppConfig):
     name = 'object_history'
     def ready(self):
-        post_migrate.connect(on_post_migrate, sender=self)
+        import object_history.signals
