fea2bc5b0f8354fc1074de1e5f81e4df47d486fd
nocarryr <matt@nomadic-recording.com>
2016-02-04 16:26:21 -0600

    Add signal handler to instances to their default categories

---
+++ avam/assettypes/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'assettypes.apps.AssetTypesConfig'
--- avam/assettypes/apps.py
+++ avam/assettypes/apps.py
@@ -1,5 +1,17 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save
 
+def on_asset_model_post_save(sender, **kwargs):
+    if kwargs.get('raw'):
+        return
+    instance = kwargs.get('instance')
+    instance.add_model_category_defaults()
 
 class AssetTypesConfig(AppConfig):
     name = 'assettypes'
+    def ready(self):
+        ModelBase = self.models_module.ModelBase
+        for m in self.get_models():
+            if not issubclass(m, ModelBase):
+                continue
+            post_save.connect(on_asset_model_post_save, sender=m)
