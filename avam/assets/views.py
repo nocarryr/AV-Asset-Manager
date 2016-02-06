37f404c13bdec2196c4939f4f93444b139d01c2e
nocarryr <matt@nomadic-recording.com>
2016-02-06 14:39:18 -0600

    Begin adding filters for asset list (not fully implemented yet)

--- avam/assets/views.py
+++ avam/assets/views.py
@@ -9,8 +9,12 @@ class AssetList(LoginRequiredMixin, ListView):
     model = Asset
     template_name = 'assets/asset-list.html'
     context_object_name = 'asset_list'
+    def get_manufacturers(self, context):
+        assets = [asset.asset_instance for asset in context['asset_list']]
+        return set([asset.asset_model.manufacturer for asset in assets])
     def get_context_data(self, **kwargs):
         context = super(AssetList, self).get_context_data(**kwargs)
+        context['manufacturers'] = self.get_manufacturers(context)
         context['asset_item_fields'] = [
             'asset_model',
             'location',
