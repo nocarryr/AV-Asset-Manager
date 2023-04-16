56e70e8f732508f76dc1ed39ed8ff059ccd9a1a6
nocarryr <matt@nomadic-recording.com>
2023-04-16 15:54:52 -0500

    Add github test workflow

--- tests.py
+++
@@ -1,8 +0,0 @@
-def test():
-    import os
-    import subprocess
-    import shlex
-    cwd = os.path.dirname(os.path.abspath(__file__))
-    os.chdir(os.path.join(cwd, 'avam'))
-    cmd = 'python manage.py test'
-    print(subprocess.check_output(shlex.split(cmd)))
